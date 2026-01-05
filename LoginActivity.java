ad5f98846a05642940c0a2eed3b3f61396a41df4
Muhammad Waseem <waseemrawn5@gmail.com>
2026-01-05 10:10:04 +0500

    Add files via upload

---
+++ LoginActivity.java
@@ -0,0 +1,8 @@
+import android.content.*;import android.os.*;import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+public class LoginActivity extends AppCompatActivity{
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.activity_login);
+    }
+}
